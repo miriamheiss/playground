93a934ba916c2694d8ca7a8d66151abc8c648fb2
miriamheiss <memerheiss@gmail.com>
2022-12-13 17:39:50 -0500

    Art stuff

--- Untitled2.R
+++ Untitled2.R
@@ -13,3 +13,10 @@ random_variable <- load_variables(year = 2021, dataset = "acs5")
 # B01001 is sex, age and race
 
 get_estimates(geography = "county", product = "components", state = "13", county = "Gwinnett", year = "2020")
+
+maine_counties <- get_acs(state = "Maine", geography = "county", variables = c(hhimcome = "B19013_001"), year = 2021)
+
+
+maine_counties %>% 
+ggplot(mapping = aes(x = estimate, y = reorder(NAME, estimate))) +
+  geom_pointrange(aes(xmax = estimate + moe, xmin = estimate - moe))
